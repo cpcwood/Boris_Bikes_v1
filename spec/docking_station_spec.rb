bf610c8a9e4c5b4dad8b5e52ff14d5bbfbdb1346
cpcwood <cpcwood@outlook.com>
2019-12-10 17:05:49 +0000

    Added test to check whether DockingStation instance gets bike which works

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,20 @@ require 'boris_bikes'
 describe DockingStation do
   it { should respond_to(:release_bike)}
 end
+
+describe Bike do
+  it { should respond_to(:working?)}
+end
+
+
+
+describe 'Gets bike and expects it to be working' do
+
+  docking_station = DockingStation.new
+  bike = docking_station.release_bike
+
+  it 'bike working should return true' do
+    expect(bike.working?).to eq true
+  end
+
+end
